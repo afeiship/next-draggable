1972d086a2fcfe7e67413742506c1957552554df
afeiship <1290657123@qq.com>
2019-05-08 16:08:46 +0800

    add: touch events support

--- dist/next-draggable.js
+++ dist/next-draggable.js
@@ -1,3 +1,10 @@
+/*!
+ * name: next-draggable
+ * url: https://github.com/afeiship/next-draggable
+ * version: 1.0.0
+ * license: MIT
+ */
+
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const portNumber = 3000;
+
+app.use(express.static('node_modules'));
+app.use(express.static('src'));
+app.use(express.static('test'));
+
+app.listen(portNumber, () => {
+  console.log(`Express web server started: http://localhost:${portNumber}`);
+  console.log(`Serving content from /src/`);
+});
